f3c2eae03db543cb6375bf919d1ff43df0de21cc
abbas5152 <104075776+abbas5152@users.noreply.github.com>
2022-07-08 23:00:32 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package day9;
+
+public class Main {
+      public static void main(String[] args) {
+		Area a1=new Area();
+		System.out.println(a1.circleArea(6));
+		System.out.println(a1.rectangleArea(5, 5));
+		System.out.println(a1.squareArea(5));
+	}
+}
